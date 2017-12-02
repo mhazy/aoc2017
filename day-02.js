a7d067b3b62119a1a169625ed192b7173bdb5ff9
Mark Hayes <mark.hayes@rangle.io>
2017-12-02 14:32:10 -0500

    Day 2, part two

--- day-02.js
+++ day-02.js
@@ -12,13 +12,40 @@ const calculateRowChecksum = R.compose(
   R.applySpec([rowMaximum, rowMinimum])
 );
 
-const calculateSheetChecksum = R.compose(
-  R.sum,
-  R.map(calculateRowChecksum),
-  parseInput
+const createChecksumFn = fn => R.compose(R.sum, R.map(fn), parseInput);
+const isInteger = num => num === parseInt(num);
+
+const findDivisibles = row => {
+  const sorted = R.sort((a, b) => b - a, row);
+  const len = sorted.length;
+  for (let i = 0; i < len; i++) {
+    for (let j = len - 1; j >= 0; j--) {
+      const divided = sorted[i] / sorted[j];
+      if (i !== j && isInteger(divided)) {
+        return [sorted[i], sorted[j]];
+      }
+    }
+  }
+
+  return false;
+};
+
+const calculateDivisibleRowChecksum = R.compose(
+  R.apply(R.divide),
+  findDivisibles
+);
+
+const calculateSheetChecksum = createChecksumFn(calculateRowChecksum);
+
+const calculateDivisibleSheetChecksum = createChecksumFn(
+  calculateDivisibleRowChecksum
 );
 
 module.exports = {
+  findDivisibles,
+  calculateDivisibleRowChecksum,
+  calculateDivisibleSheetChecksum,
+  isInteger,
   rowMaximum,
   rowMinimum,
   parseInput,
