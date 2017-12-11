b80ea31cf3cc1dff61029dcf928dff220542ac9a
Mark Hayes <mark.hayes@rangle.io>
2017-12-11 01:05:25 -0500

    hashity hash day 10

--- day-10.js
+++ day-10.js
@@ -1,21 +1,88 @@
+const R = require("ramda");
 const parseInput = input => input.split(/,/).map(Number);
 
 class KnotHasher {
-  constructor(length) {
-    this._length = length;
+  constructor(isBasic) {
+    this._length = isBasic ? 5 : 256;
     this._list = null;
     this._cursor = 0;
     this._skip = 0;
-    this.init();
+
+    this._inputAppend = [17, 31, 73, 47, 23];
+    this.reset();
   }
 
-  init() {
+  reset() {
     this._list = [...Array(this._length).keys()];
+    this._cursor = 0;
+    this._skip = 0;
+  }
+
+  hash(input) {
+    const inputArray = this.convertInputToAsciiArray(input);
+    let i = 0;
+    while (i < 64) {
+      let j = 0;
+      const len = inputArray.length;
+      while (j < len) {
+        this.rotate(inputArray[j]);
+        j = j + 1;
+      }
+      i = i + 1;
+    }
+
+    i = 0;
+    let hash = "";
+    while (i < 16) {
+      let val = this._list[i * 16];
+      let j = i * 16 + 1;
+      let h = i * 16 + 16;
+      while (j < h) {
+        val = val ^ this._list[j];
+        j = j + 1;
+      }
+      const stringVal = (val <= 16 ? "0" : "") + val.toString(16);
+      hash = hash + stringVal;
+
+      i = i + 1;
+    }
+    // Reset the hasher
+    this.reset();
+    return hash;
+  }
+
+  convertInputToAsciiArray(input) {
+    return input
+      .split("")
+      .reduce((acc, val) => acc.concat(val.charCodeAt(0)), [])
+      .concat(this._inputAppend);
+  }
+
+  index(val) {
+    return val % this._list.length;
   }
 
   rotate(val) {
-    this._cursor = this._cursor + val + this._skip;
+    if (val > this._list.length) {
+      return;
+    }
+    let i = 0;
+    let len = Math.floor(val / 2);
+    const last = this._cursor + val;
+    while (i < len) {
+      const firstIndex = this.index(this._cursor + i);
+      const lastIndex = this.index(this._cursor + (val - 1) - i);
+      const holdVal = this._list[firstIndex];
+      this._list[firstIndex] = this._list[lastIndex];
+      this._list[lastIndex] = holdVal;
+      i = i + 1;
+    }
+    this._cursor = this.index(this._cursor + val + this._skip);
     this._skip = this._skip + 1;
+  }
+
+  firstTwoMultiplied() {
+    return this._list[0] * this._list[1];
   }
 
   get list() {
--- day-10.test.js
+++ day-10.test.js
@@ -1,23 +1,98 @@
 const lib = require("./day-10");
+const puzzleInput = "129,154,49,198,200,133,97,254,41,6,2,1,255,0,191,108";
 
 describe("Day 10 - Knot hash", () => {
-  let hasher;
+  describe("Tests", () => {
+    let hasher;
 
-  it("parses input", () => {
-    expect(lib.parseInput("1,2,3")).toMatchObject([1, 2, 3]);
+    it("parses input", () => {
+      expect(lib.parseInput("1,2,3")).toMatchObject([1, 2, 3]);
+    });
+
+    beforeEach(() => {
+      hasher = new lib.KnotHasher(true);
+    });
+
+    it("has a list of the specified length", () => {
+      expect(hasher.list).toMatchObject([0, 1, 2, 3, 4]);
+    });
+
+    it("increases the cursor and step after rotating", () => {
+      hasher.rotate(3);
+      expect(hasher.skip).toBe(1);
+      expect(hasher.cursor).toBe(3);
+    });
+
+    it("ignores values larger than the list", () => {
+      hasher.rotate(10);
+      expect(hasher.skip).toBe(0);
+      expect(hasher.cursor).toBe(0);
+    });
+
+    it("returns the wrapped index value", () => {
+      expect(hasher.index(5)).toBe(0);
+      expect(hasher.index(6)).toBe(1);
+      expect(hasher.index(1)).toBe(1);
+    });
+
+    it("rotates the list by the specified amount", () => {
+      hasher.rotate(3);
+      expect(hasher.list).toMatchObject([2, 1, 0, 3, 4]);
+
+      hasher.rotate(4);
+      expect(hasher.list).toMatchObject([4, 3, 0, 1, 2]);
+
+      hasher.rotate(1);
+      expect(hasher.list).toMatchObject([4, 3, 0, 1, 2]);
+
+      hasher.rotate(5);
+      expect(hasher.list).toMatchObject([3, 4, 2, 1, 0]);
+
+      expect(hasher.firstTwoMultiplied()).toBe(12);
+    });
   });
 
-  beforeEach(() => {
-    hasher = new lib.KnotHasher(5);
+  describe("Part 2", () => {
+    it("converts the input to an array", () => {
+      const hasher = new lib.KnotHasher();
+      expect(hasher.convertInputToAsciiArray("1,2,3")).toMatchObject([
+        49,
+        44,
+        50,
+        44,
+        51,
+        17,
+        31,
+        73,
+        47,
+        23
+      ]);
+    });
+
+    it("creates a hash", () => {
+      const hasher = new lib.KnotHasher();
+      expect(hasher.hash("")).toBe("a2582a3a0e66e6e86e3812dcb672a272");
+      expect(hasher.hash("AoC 2017")).toBe("33efeb34ea91902bb2f59c9920caa6cd");
+      expect(hasher.hash("1,2,3")).toBe("3efbe78a8d82f29979031a4aa0b16a9d");
+      expect(hasher.hash("1,2,4")).toBe("63960835bcdc130f0b66d7ff4f6a5a8e");
+    });
   });
 
-  it("has a list of the specified length", () => {
-    expect(hasher.list).toMatchObject([0, 1, 2, 3, 4]);
-  });
+  describe("Solution", () => {
+    it("solves part 1", () => {
+      const hasher = new lib.KnotHasher();
+      const parsedInput = puzzleInput.split();
+      const input = lib.parseInput(puzzleInput);
+      input.forEach(val => {
+        hasher.rotate(val);
+      });
+      expect(hasher.firstTwoMultiplied()).toBe(19591);
+    });
 
-  it("increases the cursor and step after rotating", () => {
-    hasher.rotate(3);
-    expect(hasher.skip).toBe(1);
-    expect(hasher.cursor).toBe(3);
+    it("solves part 2", () => {
+      const hasher = new lib.KnotHasher();
+      const result = hasher.hash(puzzleInput);
+      expect(result).toBe("62e2204d2ca4f4924f6e7a80f1288786");
+    });
   });
 });
